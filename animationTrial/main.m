4a4f050715436a0bc8ed87c56c13f0445c4403fe
vikas bansal <bansal.vks@gmail.com>
2015-08-13 17:31:05 +0530

    NSImageView hoping to random location issue resolved
    On animation begin the NSImageView was getting moved form its place.
    Issue resolved

---
+++ animationTrial/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  animationTrial
+//
+//  Created by Paxcel on 13/08/15.
+//  Copyright (c) 2015 TechHeal. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
